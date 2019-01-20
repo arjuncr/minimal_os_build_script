013d37df37063344ca6e792ee49d994ada1fd157
ARJUN <arjuncr00@gmail.com>
2019-01-20 16:39:10 +0530

    fix in script

--- download_1.sh
+++ download_1.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-. ./env.sh
+./env.sh
 
 wget -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v4.x/linux-$KERNEL_VERSION.tar.xz
 wget -O busybox.tar.bz2 http://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
 wget -O syslinux.tar.xz http://kernel.org/pub/linux/utils/boot/syslinux/syslinux-$SYSLINUX_VERSION.tar.xz
-
-
