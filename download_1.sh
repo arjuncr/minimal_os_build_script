f785bf0abc67d0d378828376bca743e2215fdd86
ARJUN C R <arjuncr00@gmail.com>
2020-06-27 17:38:39 +0100

    Update download_1.sh

--- download_1.sh
+++ download_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./env.sh
 
-wget -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v4.x/linux-$KERNEL_VERSION.tar.xz
+wgt  -O kernel.tar.xz https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-$KERNEL_VERSION.tar.xz
 wget -O busybox.tar.bz2 http://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
 wget -O syslinux.tar.xz http://kernel.org/pub/linux/utils/boot/syslinux/syslinux-$SYSLINUX_VERSION.tar.xz
